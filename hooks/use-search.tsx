4b35a482ec8516381a08a554ea0b81819e36743a
Diky Arif Febriyanto <dikyariff97@gmail.com>
2023-11-16 17:00:27 +0700

    fix: fix deployment issue

--- hooks/use-search.tsx
+++ hooks/use-search.tsx
@@ -4,7 +4,7 @@ type SearchStore = {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
-  toggle: () => Void;
+  toggle: () => void;
 };
 
 export const useSearch = create<SearchStore>((set, get) => ({
